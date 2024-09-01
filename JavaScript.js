9ff4a2f273abbfb05c645a0f2c289504222eb32a
cas1092 <castillo.goche.l@gmail.com>
2024-09-01 00:54:21 -0500

    Initial commit

---
+++ JavaScript.js
@@ -0,0 +1,19 @@
+﻿const button = document.querySelector(".arrow");
+const popup = document.querySelector(".share");
+const icn = document.getElementById("next");
+icn.setAttribute("fill", "#6E8098");
+
+popup.classList.add("hidden");
+button.classList.add("arrow2");
+
+button.addEventListener("click", () => {
+    popup.classList.toggle("hidden");
+    button.classList.toggle("change_color");
+
+    if (popup.classList.contains("hidden")) {
+        icn.setAttribute("fill", "#6E8098");
+    }
+    else {
+        icn.setAttribute("fill", "#6A5D48");
+    }
+})
